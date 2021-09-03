80390d4497b6b9c65a0923344045b3343ecbf33d
Pratham Verma <prathamverma12345@gmail.com>
2021-09-03 11:15:19 +0530

    Home Page Detail made

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test/pages/home_page.dart';
 import 'package:test/pages/login_page.dart';
-// import 'package:google_fonts/google_fonts.dart';
 import 'package:test/utils/routes.dart';
 import 'package:test/widgets/themes.dart';
 
@@ -12,30 +11,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Variable types
-    // int String double bool num var const final
-    // var color = "dark";
-
     return MaterialApp(
-      // home: Homepage(),
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
-      //    ThemeData(
-      //   primarySwatch: Colors.deepPurple,
-      //   fontFamily: GoogleFonts.lato().fontFamily,
-      //   // primaryTextTheme: GoogleFonts.latoTextTheme(),
-      // ),
-      // darkTheme: ThemeData(
-      //   // brightness: Brightness.dark,
-      //   primarySwatch: Colors.blue,
-      // ),routes
       initialRoute: MyRoutes.homeRoute,
       routes: {
-        // "/": (context) => Homepage(),
         MyRoutes.loginRoute: (context) => Loginpage(),
-        MyRoutes.homeRoute: (context) => HomePage()
+        MyRoutes.homeRoute: (context) => HomePage(),
       },
     );
   }
